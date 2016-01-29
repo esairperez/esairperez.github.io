64b46d6b6b169053bacd04c0a03d30ba2f7821b8
esairperez <esairperez10@gmail.com>
2016-01-28 21:33:52 -0600

    created php files

---
+++ projects/biz-php/contact.php
@@ -0,0 +1,11 @@
+
+<form name="contactform" action="emailform.php" method="post" enctype="text/plain">
+Recipient:<br>
+<input type="text" name="name" value=""><br>
+Subject:<br>
+<input type="text" name="mail" value=""><br>
+Message:<br>
+<input type="text" name="Message" value="" size="50"><br><br>
+<input type="submit" value="Send">
+<input type="reset" value="Reset">
+</form>
